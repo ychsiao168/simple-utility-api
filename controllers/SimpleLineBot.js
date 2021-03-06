3119b04f7f504fc6d0640c735f2ff2dfd4edcd07
Yung Chung Hsiao <yungchunghsiao@protonmail.com>
2021-03-06 23:29:06 +0800

    [linebot] use more postback

--- controllers/SimpleLineBot.js
+++ controllers/SimpleLineBot.js
@@ -50,7 +50,6 @@ export const handleLineBot = (req, res) => {
 const handleEvent = async (event) => {
 
   let returnMsg = {}
-  const { userId } = event.source
 
   if (!((event.type === "message" && event.message.type === "text") || (event.type === "postback"))) {
     // 目前僅過濾文字+POSTBACK
@@ -79,85 +78,108 @@ const handleEvent = async (event) => {
     case /天氣\s*(\d+)/.test(event?.message?.text): {
       //console.log("天氣", RegExp.$1)
       const locationIndex = RegExp.$1
-      const records = await gWeather.getRecords(locationIndex)
-      if (records === null) {
-        returnMsg.type = "text"
-        returnMsg.text = "地區編號錯誤"
-      } else {
-        returnMsg = new Array(
-          {
-            type: "text",
-            text: records[0].locationName,
-          },
-          createCarouselMessage(records, records[0].locationName + "天氣預報", createWeatherMessage),
-        )
-      }
+      returnMsg = await _processWX(Number(locationIndex))
       break
     }
-
 
     case /空氣\s*(\d+)\s*(\d*)/.test(event?.message?.text): {
       //console.log("空氣", RegExp.$1, RegExp.$2)
       const locationIndex = RegExp.$1
-      const records = await gAQIData.getRecords(locationIndex)
-
-      if (records === null) {
-        returnMsg.type = "text"
-        returnMsg.text = "地區編號錯誤"
-      } else {
-
-        if (records.length === 1) {
-          returnMsg = createAQIMessage(records[0])
-        } else {
-          returnMsg = new Array(
-            {
-              type: "text",
-              text: `${records[0]["County"]} 空氣品質`,
-            },
-            createCarouselMessage(records.slice(0, 10), `${records[0]["County"]} 空氣品質`, createAQIMessage),
-          )
-
-          if (records.length >= 10) {
-            returnMsg.push(
-              createCarouselMessage(records.slice(10,), `${records[0]["County"]} 空氣品質`, createAQIMessage)
-            )
-          }
-        }
-      }
-
+      returnMsg = await _processAQ(Number(locationIndex))
       break
     }
 
     case /postback/.test(event.type): {
-      const data = querystring.parse(event.postback.data);
-      switch (data.menu) {
-        case "Home": client.linkRichMenuToUser(userId, MENU_MAIN); break
-        case "WX_ZONES": client.linkRichMenuToUser(userId, MENU_WX_ZONES); break
-        case "AQ_ZONES": client.linkRichMenuToUser(userId, MENU_AQ_ZONES); break
-
-        case "WX_NorthZone": client.linkRichMenuToUser(userId, MENU_WX_NORTHZONE); break
-        case "WX_MiddleZone": client.linkRichMenuToUser(userId, MENU_WX_MIDDLEZONE); break
-        case "WX_SouthZone": client.linkRichMenuToUser(userId, MENU_WX_SOUTHZONE); break
-        case "WX_EastZone": client.linkRichMenuToUser(userId, MENU_WX_EASTZONE); break
-        case "WX_IslandZone": client.linkRichMenuToUser(userId, MENU_WX_ISLANDZONE); break
-
-        case "AQ_NorthZone": client.linkRichMenuToUser(userId, MENU_AQ_NORTHZONE); break
-        case "AQ_MiddleZone": client.linkRichMenuToUser(userId, MENU_AQ_MIDDLEZONE); break
-        case "AQ_SouthZone": client.linkRichMenuToUser(userId, MENU_AQ_SOUTHZONE); break
-        case "AQ_EastZone": client.linkRichMenuToUser(userId, MENU_AQ_EASTZONE); break
-        case "AQ_IslandZone": client.linkRichMenuToUser(userId, MENU_AQ_ISLANDZONE); break
-
-        default: client.linkRichMenuToUser(userId, MENU_MAIN); break
-      }
-      return Promise.resolve(null);
+      const ret = await _handleEventPostback(event)
+      if (ret === null) return Promise.resolve(null)
+      else returnMsg = ret
+      break
     }
 
     default:
       // returnMsg = MainMenuMessage
       returnMsg.type = "text"
       returnMsg.text = helpText
+      client.setDefaultRichMenu(MENU_MAIN)
       break
   }
 
   return client.replyMessage(event.replyToken, returnMsg);
+}
+
+const _processAQ = async (locIdx) => {
+
+  let retObj = {}
+  const records = await gAQIData.getRecords(locIdx)
+
+  if (records === null) {
+    retObj.type = "text"
+    retObj.text = "地區編號錯誤"
+  } else {
+
+    if (records.length === 1) {
+      retObj = createAQIMessage(records[0])
+    } else {
+      retObj = new Array(
+        createCarouselMessage(records.slice(0, 10), `${records[0]["County"]} 空氣品質`, createAQIMessage)
+      )
+
+      if (records.length >= 10) {
+        retObj.push(
+          createCarouselMessage(records.slice(10,), `${records[0]["County"]} 空氣品質`, createAQIMessage)
+        )
+      }
+    }
+  }
+  return retObj
+}
+
+const _processWX = async (locIdx) => {
+  let retObj = {}
+  const records = await gWeather.getRecords(locIdx)
+  if (records === null) {
+    retObj.type = "text"
+    retObj.text = "地區編號錯誤"
+  } else {
+    retObj = new Array(
+      {
+        type: "text",
+        text: records[0].locationName,
+      },
+      createCarouselMessage(records, records[0].locationName + "天氣預報", createWeatherMessage),
+    )
+  }
+  return retObj
+}
+
+const _handleEventPostback = async (event) => {
+  const data = querystring.parse(event.postback.data);
+  const { userId } = event.source
+  let retObj = null
+
+  switch (true) {
+    case /HELPTXT/.test(data?.menu): retObj = new Object({ type: "text", text: helpText }); break
+    case /Home/.test(data?.menu): client.linkRichMenuToUser(userId, MENU_MAIN); break
+    case /WX_ZONES/.test(data?.menu): client.linkRichMenuToUser(userId, MENU_WX_ZONES); break
+    case /AQ_ZONES/.test(data?.menu): client.linkRichMenuToUser(userId, MENU_AQ_ZONES); break
+
+    case /WX_NorthZone/.test(data?.menu): client.linkRichMenuToUser(userId, MENU_WX_NORTHZONE); break
+    case /WX_MiddleZone/.test(data?.menu): client.linkRichMenuToUser(userId, MENU_WX_MIDDLEZONE); break
+    case /WX_SouthZone/.test(data?.menu): client.linkRichMenuToUser(userId, MENU_WX_SOUTHZONE); break
+    case /WX_EastZone/.test(data?.menu): client.linkRichMenuToUser(userId, MENU_WX_EASTZONE); break
+    case /WX_IslandZone/.test(data?.menu): client.linkRichMenuToUser(userId, MENU_WX_ISLANDZONE); break
+
+    case /AQ_NorthZone/.test(data?.menu): client.linkRichMenuToUser(userId, MENU_AQ_NORTHZONE); break
+    case /AQ_MiddleZone/.test(data?.menu): client.linkRichMenuToUser(userId, MENU_AQ_MIDDLEZONE); break
+    case /AQ_SouthZone/.test(data?.menu): client.linkRichMenuToUser(userId, MENU_AQ_SOUTHZONE); break
+    case /AQ_EastZone/.test(data?.menu): client.linkRichMenuToUser(userId, MENU_AQ_EASTZONE); break
+    case /AQ_IslandZone/.test(data?.menu): client.linkRichMenuToUser(userId, MENU_AQ_ISLANDZONE); break
+
+    case /AQ_(\d+)/.test(data?.action): retObj = await _processAQ(Number(RegExp.$1)); break
+    case /WX_(\d+)/.test(data?.action): retObj = await _processWX(Number(RegExp.$1)); break
+
+    default: client.linkRichMenuToUser(userId, MENU_MAIN); break
+  }
+
+  return retObj
 }
